245f5bb6bac16c73f5d3ecd0afb4a975989b5e2f
naveena2223 <114135449+naveena2223@users.noreply.github.com>
2023-02-13 17:52:13 +0530

    Add files via upload

---
+++ KafeneProject/js/login.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var loginStatus = window.localStorage.getItem("login-status");
+    loginStatus =
+      loginStatus === null || loginStatus === "" ? false : loginStatus;
+  
+    if (JSON.parse(loginStatus) == true) {
+      location.assign("/order.html");
+    }
+  });
